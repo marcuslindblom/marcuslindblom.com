116f7128fd95e13284802a8b30faedbe870c8632
Marcus Lindblom <m@marcuslindblom.com>
2022-06-28 13:02:30 +0200

    Use unpkg

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,3 +1,3 @@
-import useStrife from '../node_modules/@strifeapp/strife/dist/index.esm.js';
+import useStrife from 'https://unpkg.com/@strifeapp/strife@0.0.6-preview.12/dist/index.esm.js';
 
 useStrife();
