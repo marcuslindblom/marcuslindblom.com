fe21011f2395ff4e99aee838dcba9afc7519bffa
Marcus Lindblom <m@marcuslindblom.com>
2022-07-25 23:21:03 +0200

    Fix build

--- Models/HomeViewModel.cs
+++ Models/HomeViewModel.cs
@@ -4,8 +4,8 @@ public record HomeViewModel {
   public string? Heading { get; set; }
   public List<Post>? Posts { get; set; } = new List<Post>();
 
-  public static string InformationalVersion =>
+  public string InformationalVersion =>
     Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-  public static string Sha =>
+  public string Sha =>
   InformationalVersion.Substring(InformationalVersion.LastIndexOf("-") + 1);
 }
