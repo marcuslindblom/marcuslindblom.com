dd24d99a5b8cb29d33f5a4721379ee14181dfc56
Marcus Lindblom <m@marcuslindblom.com>
2022-08-22 19:33:48 +0200

    Test webhook

--- Controllers/WebhookController.cs
+++ Controllers/WebhookController.cs
@@ -1,53 +1,74 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using Raven.Client.Documents;
 
 namespace WebMention;
 
 [ApiController]
+[Route("[controller]")]
 public class WebhookController : ControllerBase
 {
   private readonly ILogger<WebhookController> logger;
+  private readonly IDocumentStore documentStore;
 
-  public WebhookController(ILogger<WebhookController> logger)
+  public WebhookController(ILogger<WebhookController> logger, IDocumentStore documentStore)
   {
     this.logger = logger;
+    this.documentStore = documentStore;
   }
-  [HttpPost("/webmention")]
+  [HttpGet]
+  public IActionResult Index() => Content("Hello, world");
+
+  [HttpPost("webmention")]
   public async Task<IActionResult> WebMention([FromBody] WebMentionWebhookModel data)
   {
     logger.LogInformation("User {UserId} mentioned {PostURL}",
-        data.Post.Author.Name, data.Post.Url);
-    // await _votesCache.AddNewTopggVote(data.User);
+        data.Post.Author.Name, data.Post.RepostOf);
+
+    using var session = this.documentStore.OpenAsyncSession();
+    await session.StoreAsync(data.Post, string.Empty);
+    await session.SaveChangesAsync();
     return Ok();
   }
 }
 
-public record WebMentionWebhookModel
+public class WebMentionWebhookModel
 {
-  public string Secret { get; set; }
-  public string Source { get; set; }
-  public string Target { get; set; }
-  public Post Post { get; set; }
+
+  public string? Secret { get; set; }
+
+  public Uri? Source { get; set; }
+
+  public Uri? Target { get; set; }
+
+  public Post? Post { get; set; }
 }
 
-public record Author
+public class Author
 {
-  public string Name { get; set; }
-  public string Photo { get; set; }
-  public string Url { get; set; }
+
+  public string? Name { get; set; }
+
+  public string? Photo { get; set; }
+
+  public Uri? Url { get; set; }
 }
 
-public record Post
+public class Post
 {
-  public string Type { get; set; }
-  public Author Author { get; set; }
-  public string Url { get; set; }
+
+  public string? Type { get; set; }
+
+  public Author? Author { get; set; }
+
+  public Uri? Url { get; set; }
+
   public DateTime Published { get; set; }
-  public string Name { get; set; }
 
-  [JsonProperty("repost-of")]
-  public string RepostOf { get; set; }
-
-  [JsonProperty("wm-property")]
-  public string WmProperty { get; set; }
+  public string? Name { get; set; }
+  [JsonPropertyName("repost-of")]
+  public Uri? RepostOf { get; set; }
+  [JsonPropertyName("wm-property")]
+  public string? WmProperty { get; set; }
 }
