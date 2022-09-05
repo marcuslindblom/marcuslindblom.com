6a56ad9f95afa67a57bc505a32517d34dfca690f
Marcus Lindblom <m@marcuslindblom.com>
2022-09-05 14:57:00 +0200

    Lighthouse optimization

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -2,17 +2,19 @@ import useStrife from '@strifeapp/strife';
 
 window.self !== window.top ? useStrife() : null;
 
-
 console.clear();
-const featuresEl = document.querySelector(".features");
-const featureEls = document.querySelectorAll(".feature");
 
-featuresEl.addEventListener("pointermove", (ev) => {
-  featureEls.forEach((featureEl) => {
-    // Not optimized yet, I know
-    const rect = featureEl.getBoundingClientRect();
+const featuresEl = document.querySelector('.features');
+if (featuresEl) {
+  const featureEls = document.querySelectorAll('.feature');
 
-    featureEl.style.setProperty("--x", ev.clientX - rect.left);
-    featureEl.style.setProperty("--y", ev.clientY - rect.top);
+  featuresEl.addEventListener('pointermove', (ev) => {
+    featureEls.forEach((featureEl) => {
+      // Not optimized yet, I know
+      const rect = featureEl.getBoundingClientRect();
+
+      featureEl.style.setProperty('--x', ev.clientX - rect.left);
+      featureEl.style.setProperty('--y', ev.clientY - rect.top);
+    });
   });
-});
+}
