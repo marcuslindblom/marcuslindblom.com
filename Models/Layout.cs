b7e8952bc562025de5e0335ad49e34a22f8e05e9
Marcus Lindblom <m@marcuslindblom.com>
2023-01-14 12:46:23 +0100

    Adds canonical url

--- Models/Layout.cs
+++ Models/Layout.cs
@@ -1,6 +1,10 @@
 using System.Reflection;
 
 public record Layout {
+  public Layout() {
+    CanonicalUrl = "https://marcuslindblom.com";
+  }
+  public string CanonicalUrl { get;set; }
   private string? InformationalVersion { get; init; } = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
   public string? Sha => InformationalVersion?.Substring(InformationalVersion.LastIndexOf("-") + 1);
 }
