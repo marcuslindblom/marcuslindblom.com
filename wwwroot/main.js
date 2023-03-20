184dbbd0fc321eeeb81889cc7365894e03a7a5cf
Marcus Lindblom <m@marcuslindblom.com>
2023-03-20 12:05:20 +0100

    Update SDK

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,23 +1,3 @@
 import useStrife from '@strifeapp/strife';
 
 useStrife();
-// window.self !== window.top ? useStrife() : null;
-
-// if(window.self !== window.top) useStrife();
-
-// console.clear();
-
-// const featuresEl = document.querySelector('.features');
-// if (featuresEl) {
-//   const featureEls = document.querySelectorAll('.feature');
-
-//   featuresEl.addEventListener('pointermove', (ev) => {
-//     featureEls.forEach((featureEl) => {
-//       // Not optimized yet, I know
-//       const rect = featureEl.getBoundingClientRect();
-
-//       featureEl.style.setProperty('--x', ev.clientX - rect.left);
-//       featureEl.style.setProperty('--y', ev.clientY - rect.top);
-//     });
-//   });
-// }
