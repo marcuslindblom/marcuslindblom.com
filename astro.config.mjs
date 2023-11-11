f006923e25cc2342626131512ff7114840a92a35
Marcus Lindblom <m@marcuslindblom.com>
2023-11-11 14:44:38 +0100

    Adds analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
    site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  integrations: [tailwind(),  sitemap()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
