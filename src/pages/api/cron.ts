98e4d2e8bd38c3c4195a74a4e66d3db4c3bce2e2
Marcus Lindblom <m@marcuslindblom.com>
2023-11-11 15:44:41 +0100

    Remove check

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,6 +1,3 @@
 export async function GET({params, request}) {
-  if (request.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
-    return request.status(401).end('Unauthorized');
-  }
   return new Response('Hello Cron!');
 }
