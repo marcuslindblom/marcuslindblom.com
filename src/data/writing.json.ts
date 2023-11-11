6f5f8a4de68a1109f29b33da887192043b4892d6
Marcus Lindblom <m@marcuslindblom.com>
2023-11-11 14:15:35 +0100

    WIP new design

--- src/data/writing.json.ts
+++ src/data/writing.json.ts
@@ -8,14 +8,14 @@ const one: Template = {
   link: "/journal/starting-nimble-initiatives",
   title: "Starting Nimble Initiatives",
   description: "Back to simplicity",
-  date: "10.02.2021",
+  date: "16.04.2021",
 };
 
 const two: Template = {
   link: "/journal/the-first-day-of-your-life-was-the-best-day-of-mine",
   title: "Hello, world!",
   description: "The first day of your life, was the best day of mine",
-  date: "10.02.2021",
+  date: "02.08.2021",
 };
 
 export const bytype = {
