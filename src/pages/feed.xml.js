1e08c8f1b52121f37b8c13a6777ef898a97d64be
Marcus Lindblom <m@marcuslindblom.com>
2023-11-29 09:39:55 +0100

    Try to fix issue with 500 err

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -13,28 +13,31 @@ const notes = await session
   .whereEquals('published', true)
   .orderByDescending('publishedDate')
   .selectFields(['heading', 'summary', 'text', 'publishedDate', 'url'])
-  .statistics(s => stats = s)
+  .statistics((s) => (stats = s))
   .all();
 
-// Get the query duration from the stats
-const queryDuration = stats.durationInMs;
-const lastQueryTime = stats.lastQueryTime;
+try {
+  // Get the query duration from the stats
+  const queryDuration = stats.durationInMs;
+  const lastQueryTime = stats.lastQueryTime;
 
-fetch('https://in.logs.betterstack.com', {
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer A7KB9aGyb4mzyWohkx7dbJkP'
-  },
-  method: 'POST',
-  body: JSON.stringify({
-    dt: lastQueryTime,
-    queryDuration: queryDuration,
-    message: 'Query duration for feed.xml is ' + queryDuration + ' ms'
-  })
-});
+  fetch('https://in.logs.betterstack.com', {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer A7KB9aGyb4mzyWohkx7dbJkP',
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      dt: lastQueryTime,
+      queryDuration: queryDuration,
+      message: 'Query duration for feed.xml is ' + queryDuration + ' ms',
+    }),
+  });
+} catch (error) {
+  console.log('Error logging query duration: ' + error);
+}
 
 export async function GET(context) {
-
   return rss({
     title: 'Marcus Lindblom',
     description: 'RSS Feed for Marcus Lindblom',
