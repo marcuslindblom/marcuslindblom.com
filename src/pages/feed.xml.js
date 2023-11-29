80592e76154013ea99a1449c9a3ffb66e1ec611b
Marcus Lindblom <m@marcuslindblom.com>
2023-11-29 09:07:00 +0100

    Added query statistics

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -4,13 +4,34 @@ import store from '../store';
 
 const session = store.openSession();
 
+// Define an output param for getting the query stats
+let stats;
+
 const notes = await session
   .query({ indexName: 'Content/ByUrl' })
   .whereStartsWith('url', '/journal/')
   .whereEquals('published', true)
   .orderByDescending('publishedDate')
   .selectFields(['heading', 'summary', 'text', 'publishedDate', 'url'])
+  .statistics(s => stats = s)
   .all();
+
+// Get the query duration from the stats
+const queryDuration = stats.durationInMs;
+const lastQueryTime = stats.lastQueryTime;
+
+fetch('https://in.logs.betterstack.com', {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer A7KB9aGyb4mzyWohkx7dbJkP'
+  },
+  method: 'POST',
+  body: JSON.stringify({
+    dt: lastQueryTime,
+    queryDuration: queryDuration,
+    message: 'Query duration for feed.xml is ' + queryDuration + ' ms'
+  })
+});
 
 export async function GET(context) {
 
