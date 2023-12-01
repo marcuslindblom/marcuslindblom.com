7740f643a9b3fdc4c6cba61fcc665e0d5a80d672
Marcus Lindblom <m@marcuslindblom.com>
2023-12-01 15:56:07 +0100

    Adds astro lit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,24 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import lit from "@astrojs/lit";
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://marcuslindblom.com',
-  integrations: [tailwind(),  sitemap()],
+  site: 'https://marcuslindblom.com',
+  integrations: [tailwind(), sitemap(), lit()],
   output: 'server',
+  vite: {
+    ssr: {
+      noExternal: ['@strifeapp/strife']
+    }
+  },
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
