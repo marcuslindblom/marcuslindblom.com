6dd48d0d2030368fcf12eb4e513d7002aa7cc5be
Marcus Lindblom <m@marcuslindblom.com>
2024-09-07 11:48:23 +0200

    Adds Codepen link

--- src/data/contact.json.ts
+++ src/data/contact.json.ts
@@ -2,25 +2,31 @@ export interface Template {
   link: string;
   type: string;
   title: string;
-};
+}
 const one: Template = {
-  link: "mailto:m@marcuslindblom.com",
-  type: "Email",
-  title: "m@marcuslindblom.com",
+  link: 'mailto:m@marcuslindblom.com',
+  type: 'Email',
+  title: 'm@marcuslindblom.com',
 };
 const two: Template = {
-link: "https://twitter.com/marcus_lindblom",
-    type: "X",
-  title: "marcus_lindblom",
+  link: 'https://twitter.com/marcus_lindblom',
+  type: 'X',
+  title: 'marcus_lindblom',
 };
 const three: Template = {
-link: "https://read.cv/join/marcuslindblom",
-    type: "Read CV",
-  title: "marcuslindblom",
+  link: 'https://codepen.io/marcuslindblom',
+  type: 'Codepen',
+  title: 'marcuslindblom',
+};
+const four: Template = {
+  link: 'https://read.cv/join/marcuslindblom',
+  type: 'Read CV',
+  title: 'marcuslindblom',
 };
 export const bytype = {
   one,
-    two,
-    three,
+  two,
+  three,
+  four,
 };
 export const contact = Object.values(bytype);
